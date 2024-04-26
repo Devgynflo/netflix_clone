9ff6273ae245fd104875b34a47ee81d15d971932
gynflo <ginet.florian.dev@gmail.com>
2024-04-26 09:00:33 +0200

    Fix: Change bcrypt library to bcryptjs

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,8 +1,9 @@
-import {useCurrentUser} from "@/hooks/useCurrentUser";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+
 export async function getServerSideProps(context: NextPageContext) {
   const session = getSession(context);
   if (!session) {
